6752edb665bac9575995305b639741cf27e7e6cd
Malgorzata Polit-Zagun <m.polit.zagun@gmail.com>
2020-08-24 23:42:38 +0200

    Zadanie: Hello World!

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {                                          //class def.
+    public static void main(String[] args){                        // method main def.
+        System.out.println("Hello from First Class!");             // run class out.method println
+        System.out.println(9);                                     // int
+        System.out.println(true);                                  // boolean
+        System.out.println("Hello");                               // string
+        System.out.println('A');                                   // char
+    }
+}
