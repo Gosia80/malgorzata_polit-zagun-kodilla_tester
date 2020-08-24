471b92bc404f4761107959003247fba6d8b34895
Malgorzata Polit-Zagun <m.polit.zagun@gmail.com>
2020-08-25 00:28:10 +0200

    Zadanie: Zmienne

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        String myName = "Małgosia";
+        int myAge = 40;
+        double height = 1.64;
+        char carClass = 'S';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+
+        String hello = "Hello from First Class!";
+        System.out.println(hello);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String title = "As always";
+        String author = "Miłoszewski";
+        char firstDigitName = 'Z';
+        int pageNo = 452;
+        double index = pageNo/100;
+        boolean isGoodBook = true;
+
+        System.out.println("Pls be encouraged to read '"+title+"' by "+firstDigitName+". "+author+".");
+        System.out.println("Number of pages: "+pageNo);
+        System.out.println("Index: "+index);
+        System.out.println("Recommended: "+isGoodBook);
+    }
+}
